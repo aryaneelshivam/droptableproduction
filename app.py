6ac7cd607863d46130bbb246b7bc71422dc51a3f
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-29 19:44:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_icon="💧"
 )
 
-llm = OpenAI(api_token="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")
+llm = OpenAI(api_token="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")
 
 st.title(":blue[Drop]Table")
 tab = ui.tabs(options=['Local file', 'Google sheets', 'Airtable', 'Snowflake'], default_value='Local file', key="select")
