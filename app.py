f763a3b6fd52a4297d3fe4fa57c230287bcfd479
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-29 20:01:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if tab == "Local file":
 		df = pd.read_csv(uploaded_file, encoding='latin-1')
 		df = SmartDataframe(df, config={"llm": llm})
 		with st.spinner("Generating Summary..."):
-			ans = df.chat('What are the vlauable insights that can be driven out?')
+			ans = df.chat('Write a detailes report about the insights that can be driven out')
 			with card_container():
 				st.write(ans)
 
