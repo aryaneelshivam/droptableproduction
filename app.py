069aa5e54ff1e1039f80138581c82373f0792b3b
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-03-10 12:31:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ if tab == "Local file":
 				response1 = query_engine.query("Analyse the dataset, and drive valuable insights and write a detailed report, the different visualizations, different insightfu; indicators etc.")
 		if response1:
 			with st.spinner("Generating visualizations..."):
-				plot = query_engine.query("Generate full Python executable code using pandas, to create three distinct subplotted charts from the dataset. Only executable code. Take column and row names from the given dataset. subplot 6 different charts for better understanding of the given data.")
+				plot = query_engine.query("Generate full Python executable code using pandas, to plot multiple distinct charts from the dataset. Only executable code. Take column and row names from the given dataset. subplot 6 different charts for better understanding of the given data.")
 			with card_container():
 				st.markdown(response2)
 			with card_container():
