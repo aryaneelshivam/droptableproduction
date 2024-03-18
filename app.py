fd2ddf68e0ff883bc1c3c1a07df39c3fedec73dd
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-03-18 11:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ with tab1:
 			with st.expander("👀 Analyse charts and graphs visually."):
 				st.write("Sample")
 			output = st.text_area("Your generated output 🎉", placeholder="The output will be displayed here", value=conv if 'conv' in locals() else "")
-			generate, refresh = st.button("Generate AI report ⚡", "Clear all 🔄")
+			generate = st.button("Generate AI report ⚡")
 			
 		if generate:
 			query_engine = PandasQueryEngine(df=df, verbose=True, synthesize_response=True)
