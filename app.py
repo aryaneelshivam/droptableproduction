ce9490d1f5e658fb5ee0e7f250185c6b4567d2f5
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-03-20 12:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,11 @@ if tab == "Local file":
 						st.error("Having trouble displaying generated plot")
 		if manual:
 			datatypes = st.sidebar.checkbox("List datatypes from the dataset?")
+			describe = st.sidebar.checkbox("Describe the uploaded dataset?")
 			if datatypes:
 				st.sidebar.write(df.dtypes)
+			if describe:
+				st.write(df.describe(percentiles, include, exclude, datetime_is_numeric))
 			chart_types = st.multiselect("Select Chart Types", ["Bar Chart", "Stacked Bar Chart","Line Chart", "Scatter Plot", "Pie Chart", "Dot Plot", "Histogram", "Area Chart"])
 			for chart_type in chart_types:
 				st.subheader(f"{chart_type} Visualization")
